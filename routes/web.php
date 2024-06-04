37999411b1015b08c641760d8686eac4e99fac27
muhamad-tanahmerdeka7 <muhay990@gmail.com>
2024-06-04 16:12:18 -0700

    CRUD

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
